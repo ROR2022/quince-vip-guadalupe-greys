255074a69bfccbd6ce69b64ea33c66e76d87b7c0
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2026-07-29 09:51:12 -0600

    numero confirmacion

--- components/sections/data/main-data.ts
+++ components/sections/data/main-data.ts
@@ -216,7 +216,7 @@ export const quinceMainData = {
     backgroundImage: "/images/countdown-bg.jpg",
   },
   attendance: {
-    whatsappNumber: "5217444025882", //+52 1 744 402 5882
+    whatsappNumber: "5218124409916", //+52 1 81 2440 9916
     title: "CONFIRMACIÓN DE ASISTENCIA",
     message: "Respetuosamente",
     subtitle: "Confirmar antes del evento.",
